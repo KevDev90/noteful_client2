25cf43a17295b58d75c924380649532264b4a0ed
KevDev90 <kevinj902302@gmail.com>
2021-03-23 16:11:32 +0000

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    API_ENDPOINT: 'http://localhost:8000/api',
+    // API_ENDPOINT: 'https://noteful-client-kevin.herokuapp.com/',
+    API_KEY: process.env.REACT_APP_API_KEY,
+  }
